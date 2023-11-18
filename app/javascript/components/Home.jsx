b5d9bca6a09e659b52716f9f262765cde58ce2de
jeffmun <61306390+Jumshim@users.noreply.github.com>
2023-11-17 19:05:47 -0800

    established initial css for page

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Button } from "@/app/javascript/components/ui/button";
 
 export default () => (
   <div>
-    <div>
-      <div>
-        <h1>PokePortal</h1>
-        <p>
-          A central hub for all of your Pokemon needs.
-        </p>
-        <hr/>
-      </div>
+    <div class="h-screen flex flex-col items-center justify-center space-y-8">
+      <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        PokePortal
+      </h1>
+      <p>A central hub for all of your Pokemon needs.</p>
+      <Button> Enter </Button>
     </div>
   </div>
 );

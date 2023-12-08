3696e0fc050c1a045a32cb72c8cd994661623c20
jeffmun <61306390+Jumshim@users.noreply.github.com>
2023-12-07 16:09:12 -0800

    update battle completed

--- app/javascript/components/Settings/BattleSettings.jsx
+++ app/javascript/components/Settings/BattleSettings.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import { CalendarIcon } from "@radix-ui/react-icons";
 import { Separator } from "../ui/separator";
 import * as z from "zod";
 import { useForm } from "react-hook-form";
@@ -19,7 +19,7 @@ import { Button } from "../ui/button";
 import Settings from "./Settings";
 import { Textarea } from "../ui/textarea";
 import { Avatar, AvatarImage } from "../ui/avatar";
-import { toast } from "../ui/use-toast";
+import { toast, useToast } from "../ui/use-toast";
 import { AuthContext } from "../AuthProvider";
 import { Toaster } from "../ui/toaster";
 import {
@@ -30,6 +30,190 @@ import {
   Card,
   CardFooter,
 } from "../ui/card";
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import { cn } from "@/lib/utils";
+import { format } from "date-fns";
+import { Calendar } from "../ui/calendar";
+import { ScrollArea } from "../ui/scroll-area";
+
+export function UpdateBattlePopover({ id, token }) {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button variant="outline">Update Battle</Button>
+      </PopoverTrigger>
+      <PopoverContent>
+        <ScrollArea className="flex max-h-56 flex-col" type="always">
+          <UpdateBattleForm id={id} token={token} />
+        </ScrollArea>
+      </PopoverContent>
+    </Popover>
+  );
+}
+
+const formSchema = z.object({
+  title: z.string().min(2, {
+    message: "Title must be at least 2 characters.",
+  }),
+  description: z.string().min(2, {
+    message: "Must create a description",
+  }),
+  battle_type: z.string().min(2, {
+    message: "Must select a type",
+  }),
+  date: z.date(),
+});
+
+export function UpdateBattleForm({ id, token }) {
+  const { toast } = useToast();
+  const defaultValues = {
+    title: "",
+    description: "",
+    battle_type: "",
+    date: "",
+  };
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues,
+    mode: "onChange",
+  });
+
+  function onSubmit(values) {
+    const requestBody = {
+      battle: {
+        title: values.title,
+        description: values.description,
+        battle_type: values.battle_type,
+        date: values.date,
+      },
+    };
+
+    fetch(`/battles/${id}}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(requestBody),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          return;
+        }
+        return response.json();
+      })
+      .then((data) => {
+        toast({
+          title: "Battle updated successfully",
+          description: (
+            <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+              <code className="text-white">
+                {JSON.stringify(data, null, 2)}
+              </code>
+            </pre>
+          ),
+        });
+      });
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Title</FormLabel>
+              <FormControl>
+                <Input placeholder={defaultValues.title} {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Input placeholder={defaultValues.description} {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="battle_type"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Type</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="Singles" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="Singles">Singles</SelectItem>
+                  <SelectItem value="Doubles">Doubles</SelectItem>
+                </SelectContent>
+              </Select>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="date"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Date</FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "w-[240px] pl-3 text-left font-normal",
+                        !field.value && "text-muted-foreground"
+                      )}
+                    >
+                      {field.value ? (
+                        format(field.value, "PPP")
+                      ) : (
+                        <span>Pick a date</span>
+                      )}
+                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    disabled={(date) => date < new Date("1900-01-01")}
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Update Battle</Button>
+      </form>
+    </Form>
+  );
+}
 
 export function BattleSettingCard({ battle, token }) {
   function deleteBattle() {
@@ -51,43 +235,6 @@ export function BattleSettingCard({ battle, token }) {
       });
   }
 
-  //updating the battle function 
-  function updateBattle(values) {
-    const requestBody = {
-      battle: {
-        title: "TEST",
-        description: "TEST",
-        battle_type: "DOUBLES",
-        date: "12-31-2023",
-      },
-    };
-
-    fetch(`/battles/${battle.id}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify(requestBody),
-    })
-      .then((response) => {
-        //handling errors in updating the battle 
-        if (!response.ok) {
-          throw new Error("Error in updating battle");
-        }
-        return response.json();
-    })
-    .then((data) => {
-      //set the battle with the new battle data 
-      setBattles(data);
-      console.log("Battle successfully updated"); 
-    })
-    .catch((error) => {
-      // Handle errors during the update process
-      console.error("Error updating battle:", error.message);
-    });
-  }
-
   return (
     <Card>
       <CardHeader>
@@ -104,9 +251,7 @@ export function BattleSettingCard({ battle, token }) {
         <Button onClick={deleteBattle} variant="destructive">
           Delete Battle
         </Button>
-        <Button onClick={updateBattle} >
-          Update Battle
-        </Button>
+        <UpdateBattlePopover id={battle.id} token={token} />
       </CardFooter>
     </Card>
   );
